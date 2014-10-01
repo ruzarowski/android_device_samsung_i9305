0b99fa4c059946031cfafc94373b04e1e6e0aa96
Rafael Uzarowski <rafael@uzarowski.de>
2014-10-01 15:57:09 +0200

    conditional inherit of boeffla product makefile from kernel source tree

--- slim.mk
+++ slim.mk
@@ -39,3 +39,5 @@ PRODUCT_MODEL := GT-I9305
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m3xx TARGET_DEVICE=m3 BUILD_FINGERPRINT="samsung/m3xx/m3:4.1.2/JZO54K/I9305XXBMA6:user/release-keys" PRIVATE_BUILD_DESC="m3xx-user 4.1.2 JZO54K I9305XXBMA6 release-keys"
+
+$(call inherit-product-if-exists, kernel/samsung/smdk4412/boeffla.mk)
